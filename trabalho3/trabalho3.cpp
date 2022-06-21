6668dfdbf6f29952d030d2319c4673a0eed99689
LeoBardo <45073941+LeoBardineo@users.noreply.github.com>
2022-06-20 22:52:43 -0300

    saida entrada

--- trabalho3/trabalho3.cpp
+++ trabalho3/trabalho3.cpp
@@ -9,18 +9,20 @@ int main(){
     // Entrada
     int espacoPos, vertices, arestas;
     cin >> vertices >> arestas;
-    vector<vector<int>> grafo(vertices, vector<int>(arestas));
+    vector<vector<int>> grafo(vertices);
     for (int i = 0; i < arestas; i++) {
         int u, v;
         cin >> u >> v;
         grafo[u].push_back(v);
     }
 
-    cout << "salve\n";
-
     // Saída
-    for(vector<int> l : grafo)
-        for(int a : l)
-            cout << a << endl;
+    for(int i = 0; i < grafo.size(); i++){
+        if(!grafo[i].size()) continue;
+        vector<int> adj = grafo[i];
+        cout << "grafo[" << i << "]: " << endl;
+        for(int j : adj)
+            cout << j << endl;
+    }
     return 0;
 }
